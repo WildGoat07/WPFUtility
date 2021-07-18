79a56e97c1486387fd6760f7f3b047c87ef02943
Nathan HANEN <hanen.nathan@gmail.com>
2021-07-19 00:24:37 +0200

    Create PropertyChangedPropagator.cs

---
+++ WPFUtility/PropertyChangedPropagator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Wildgoat.WPFUtility
+{
+    /// <summary>
+    /// A base class for the ViewModel that listen to every INotifyPropertyChanged property that retrigger the event as itself
+    /// </summary>
+    public abstract class PropertyChangedPropagator : INotifyPropertyChanged
+    {
+        public PropertyChangedPropagator()
+        {
+            NotifyPropertyChangedProperties = GetType().GetProperties()
+                .Where(property => property.PropertyType.GetInterface(nameof(INotifyPropertyChanged)) != null)
+                .Select<PropertyInfo, (PropertyInfo, INotifyPropertyChanged?)>(property => (property, null))
+                .ToDictionary(property => property.Item1.Name);
+        }
+
+        ~PropertyChangedPropagator()
+        {
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private Dictionary<string, (PropertyInfo, INotifyPropertyChanged?)> NotifyPropertyChangedProperties { get; }
+
+        /// <summary>
+        /// Use this method to trigger the PropertyChanged changed event in the implemented classes
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (NotifyPropertyChangedProperties.ContainsKey(propertyName))
+            {
+                var property = NotifyPropertyChangedProperties[propertyName];
+                if (property.Item2 != null)
+                    property.Item2.PropertyChanged -= OnSubPropertyChanged;
+                var value = property.Item1.GetValue(this) as INotifyPropertyChanged;
+                NotifyPropertyChangedProperties[propertyName] = (property.Item1, value);
+                if (value != null)
+                    value.PropertyChanged += OnSubPropertyChanged;
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnSubPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var property = NotifyPropertyChangedProperties.First(prop => prop.Value.Item2 == sender);
+            OnPropertyChanged($"{property.Key}.{e.PropertyName}");
+        }
+    }
+}
