5ca96ebc83ea599333964c86ef1ad23b7da673d3
Nathan HANEN <hanen.nathan@gmail.com>
2021-08-01 14:34:18 +0200

    removed my source collection, use ObservableCollection instead

---
+++ WPFUtility/Collections/CollectionSourceWrapper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Wildgoat.WPFUtility.Collections
+{
+    public class CollectionSourceWrapper<T> : IBaseCollectionSource, IEnumerable<object?>, IEnumerable, INotifyCollectionChanged
+    {
+        private List<object?> buffer;
+        private bool triggerItemChange;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="triggerItemChange"></param>
+        public CollectionSourceWrapper(ObservableCollection<T> source, bool triggerItemChange = true)
+        {
+            this.triggerItemChange = triggerItemChange;
+            buffer = new List<object?>(source.Cast<object?>());
+            if (triggerItemChange)
+                foreach (var item in source)
+                    if (item is INotifyPropertyChanged notifier)
+                        notifier.PropertyChanged += OnItemPropertyChanged;
+            source.CollectionChanged += (sender, e) =>
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        buffer.InsertRange(e.NewStartingIndex, e.NewItems.Cast<object?>());
+                        if (triggerItemChange)
+                            foreach (var item in e.NewItems)
+                                if (item is INotifyPropertyChanged notifier)
+                                    notifier.PropertyChanged += OnItemPropertyChanged;
+                        CollectionChanged?.Invoke(this, e);
+                        break;
+
+                    case NotifyCollectionChangedAction.Remove:
+                        buffer.RemoveRange(e.OldStartingIndex, e.OldItems.Count);
+                        if (triggerItemChange)
+                            foreach (var item in e.OldItems)
+                                if (item is INotifyPropertyChanged notifier)
+                                    notifier.PropertyChanged -= OnItemPropertyChanged;
+                        CollectionChanged?.Invoke(this, e);
+                        break;
+
+                    case NotifyCollectionChangedAction.Replace:
+                        for (int i = 0; i < e.NewItems.Count; ++i)
+                        {
+                            var item = e.NewItems[i];
+                            var oldItem = buffer[i + e.NewStartingIndex];
+                            buffer[i + e.NewStartingIndex] = item;
+                            if (triggerItemChange)
+                            {
+                                if (oldItem is INotifyPropertyChanged oldNotifier)
+                                    oldNotifier.PropertyChanged -= OnItemPropertyChanged;
+                                if (item is INotifyPropertyChanged newNotifier)
+                                    newNotifier.PropertyChanged += OnItemPropertyChanged;
+                            }
+                        }
+                        CollectionChanged?.Invoke(this, e);
+                        break;
+
+                    case NotifyCollectionChangedAction.Move:
+                        var range = buffer.GetRange(e.OldStartingIndex, e.OldItems.Count);
+                        buffer.RemoveRange(e.OldStartingIndex, e.OldItems.Count);
+                        var insertion = e.NewStartingIndex;
+                        if (e.NewStartingIndex > e.OldStartingIndex)
+                            insertion -= e.OldItems.Count;
+                        buffer.InsertRange(insertion, range);
+                        CollectionChanged?.Invoke(this, e);
+                        break;
+
+                    case NotifyCollectionChangedAction.Reset:
+                        var removed = buffer.ToList();
+                        if (triggerItemChange)
+                            foreach (var item in buffer)
+                                if (item is INotifyPropertyChanged notifier)
+                                    notifier.PropertyChanged -= OnItemPropertyChanged;
+                        buffer.Clear();
+                        if (e.NewItems != null)
+                            buffer.AddRange(e.NewItems.Cast<object?>());
+                        CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removed, 0));
+                        if (buffer.Any())
+                            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, buffer, 0));
+                        break;
+                }
+            };
+        }
+
+        ~CollectionSourceWrapper()
+        {
+            if (triggerItemChange)
+                foreach (var item in buffer)
+                    if (item is INotifyPropertyChanged notifier)
+                        notifier.PropertyChanged -= OnItemPropertyChanged;
+        }
+
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        IEnumerator<object?> IEnumerable<object?>.GetEnumerator() => buffer.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => buffer.GetEnumerator();
+
+        private void OnItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var index = buffer.IndexOf(sender);
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, sender, sender, index));
+        }
+    }
+}
---
+++ WPFUtility/Collections/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Wildgoat.WPFUtility.Collections
+{
+    public static class Extensions
+    {
+        public static IBaseCollectionSource AsBaseCollectionSource<T>(this ObservableCollection<T> array, bool triggerItemChanges = true)
+            => new CollectionSourceWrapper<T>(array, triggerItemChanges);
+    }
+}
