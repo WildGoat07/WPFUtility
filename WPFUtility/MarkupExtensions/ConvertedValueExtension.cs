c60b70b2e9c048807c5c363d53813a98be57d849
Nathan HANEN <hanen.nathan@gmail.com>
2021-09-03 23:20:24 +0200

    Added value converter support for any value provider

---
+++ WPFUtility/MarkupExtensions/ConvertedValueExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Wildgoat.WPFUtility.MarkupExtensions
+{
+    [MarkupExtensionReturnType(typeof(object))]
+    public class ConvertedValueExtension : MarkupExtension
+    {
+        public IValueConverter? Converter { get; set; }
+        public CultureInfo Culture { get; set; } = CultureInfo.CurrentCulture;
+        public object? Parameter { get; set; }
+        public object? Source { get; set; }
+        public Type TargetType { get; set; } = typeof(object);
+
+        public override object? ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Converter?.Convert(Source, TargetType, Parameter, Culture) ?? Source;
+        }
+    }
+}
